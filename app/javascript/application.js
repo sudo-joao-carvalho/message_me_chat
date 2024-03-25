c786ea046760fa896cbe91dd931d47d002315fd9
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2024-03-25 15:29:11 +0000

    navbar added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
+//= require semantic-ui
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import * as jquery from "jquery"
+import "semantic-ui"
 import "controllers"
+
+$(document).on('turbo:load', function() {
+    $('.ui.dropdown').dropdown()
+});
