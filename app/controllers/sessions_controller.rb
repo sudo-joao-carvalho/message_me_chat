e9a323a6ef76f63f7e15e419580eb3e75c61c130
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2024-03-25 16:55:16 +0000

    authentication working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,25 @@
 class SessionsController < ApplicationController
 
-    def index
+    def new
         
+    end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "There was something wrong with your login details"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have logged out"
+        redirect_to login_path
     end
 
 end
